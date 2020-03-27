7ed789487906bab5d82ed9d8a06fad8d429985e9
APPLE <afiqon@gmail.com>
2020-03-28 00:29:20 +0800

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t afiqon/multi-client:latest -t afiqon/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t afiqon/multi-server:latest -t afiqon/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t afiqon/multi-worker:latest -t afiqon/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push afiqon/multi-client:latest
+docker push afiqon/multi-server:latest
+docker push afiqon/multi-worker:latest
+
+docker push afiqon/multi-client:$SHA
+docker push afiqon/multi-server:$SHA
+docker push afiqon/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set images deployments/server-deployment server=afiqon/multi-server:$SHA
+kubectl set images deployments/client-deployment client=afiqon/multi-client:$SHA
+kubectl set images deployments/worker-deployment worker=afiqon/multi-worker:$SHA
